2a2feca40cf94bb066fd955a9afdc5567a619e4c
Andray G <gorbanpixels@gmail.com>
2025-10-15 16:53:35 -0400

    Add Vite entry files for Vite/React (main.tsx, index.css, vite-env.d.ts)

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element #root not found");
+createRoot(container).render(<App />);
