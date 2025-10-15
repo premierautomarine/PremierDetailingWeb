829e340813c1b09bf3477e30cdc424f45715ac0a
premierautomarine <premierautomarine@gmail.com>
2025-10-15 11:43:31 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// With a custom domain, use root base:
-export default defineConfig({
-  base: '/',
-  plugins: [react()]
-})
